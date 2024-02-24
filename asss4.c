d58e1b184726a824b2b4285e2a3589c3c3aa8f42
Debayan Ghosh <149896687+Debayan-Ghosh2005@users.noreply.github.com>
2024-02-24 12:18:46 +0530

    asss4.c

--- asss4.c
+++ asss4.c
@@ -2,11 +2,11 @@
 #include<math.h>
 int main()
 {
-	float p,r,t,si,ma,ci;
+	float p,r,t,sma,cma;
 	printf("Give the input principal ammount,rate of interest and time :- ");
 	scanf("%f%f%f",&p,&r,&t);
-	si= ((p*t*r)/100)+p;
-	ci=(p*(pow(1+(r/100),t)));
-	printf("The maturity amount using Simple interest is :- %.2f and using compund interest is :- %.2f",si,ci);
+	sma= ((p*t*r)/100)+p;
+	cma=(p*pow(1+(r/100),t));
+	printf("The maturity amount using Simple interest is :- %.2f and using compund interest is :- %.2f",sma,cma);
 	return 0;
 }
